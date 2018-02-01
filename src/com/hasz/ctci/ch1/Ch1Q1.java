e51ede9299faf10fa83eb810f8a6d0f59342831d
Mack Hasz <mack@hasz.email>
2018-01-31 21:23:07 -0500

    chapter 1 queston 1

---
+++ src/com/hasz/ctci/ch1/Ch1Q1.java
@@ -0,0 +1,22 @@
+package com.hasz.ctci.ch1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ch1Q1 {
+
+	public static Boolean isUnique(String s) {
+		Set<Character> foundCharacters = new HashSet<Character>();
+		
+		for (Character c : s.toCharArray()) {		
+			if (foundCharacters.contains(c)) {
+				return false;
+			} else {
+				foundCharacters.add(c);
+			}
+		}
+		
+		return true;
+	}
+	
+}
---
+++ src/com/hasz/ctci/test/ch1/Ch1Q1Tests.java
@@ -0,0 +1,36 @@
+package com.hasz.ctci.test.ch1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.hasz.ctci.ch1.Ch1Q1;
+
+public class Ch1Q1Tests {
+	
+	@Test
+	public void testAllUnique() {
+		assertTrue(Ch1Q1.isUnique("abcdefghijklmnop"));
+	}
+	
+	@Test
+	public void testNoUnique() {
+		assertFalse(Ch1Q1.isUnique("aaaaaaaaaaaa"));
+	}
+	
+	@Test
+	public void testEmpty() {
+		assertTrue(Ch1Q1.isUnique(""));
+	}
+	
+	@Test
+	public void testOneDuplicate() {
+		assertFalse(Ch1Q1.isUnique("aabcdefghi"));
+	}
+	
+	@Test
+	public void testManyDuplicate() {
+		assertFalse(Ch1Q1.isUnique("aabbccddeeffgghhiijj"));
+	}
+
+}
