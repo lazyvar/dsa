e6cf511dc0d2a60cf80e7eb27488dd968493e34e
Mack Hasz <mack@hasz.email>
2018-02-01 19:13:29 -0500

    urlify method

---
+++ src/com/hasz/ctci/ch1/Ch1Q3.java
@@ -0,0 +1,26 @@
+package com.hasz.ctci.ch1;
+
+public class Ch1Q3 {
+
+	public static String urlify(String s, int trueLength) {
+		char[] chars = s.toCharArray();
+		int buffer = s.length() - 1;
+		
+		for (int i = trueLength - 1; i >= 0; i--) {
+			char c = chars[i];
+			
+			if (c == ' ') {
+				chars[buffer] = '0';
+				chars[buffer - 1] = '2';
+				chars[buffer - 2] = '%';
+				buffer -= 3;
+			} else {
+				chars[buffer] = c;
+				buffer--;
+			}
+		}
+		
+		return new String(chars);
+	}
+	
+}
