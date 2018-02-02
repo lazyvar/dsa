0e8d9259af97caba9d7ecfd11f6f75280b718b3d
Mack Hasz <mack@hasz.email>
2018-02-01 19:09:43 -0500

    test expected on left

--- src/com/hasz/ctci/test/ch1/Ch1Q3Tests.java
+++ src/com/hasz/ctci/test/ch1/Ch1Q3Tests.java
@@ -4,31 +4,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.hasz.ctci.ch1.Ch1Q3;
+
 public class Ch1Q3Tests {
 
 	@Test
 	public void testBookExample() {
-		assertEquals(Ch1Q3.urlify("Mr John Smith    ", 13), "Mr%20John%20Smith");
+		assertEquals("Mr%20John%20Smith", Ch1Q3.urlify("Mr John Smith    ", 13));
 	}
 
 	@Test
 	public void testSingle() {
-		assertEquals(Ch1Q3.urlify("hello there  ", 11), "hello%20there");
+		assertEquals("hello%20there", Ch1Q3.urlify("hello there  ", 11));
 	}
 	
 	@Test
 	public void testMultiple() {
-		assertEquals(Ch1Q3.urlify("what is in a name?        ", 18), "what%20is%20in%20a%20name?");
+		assertEquals("what%20is%20in%20a%20name?", Ch1Q3.urlify("what is in a name?        ", 18));
 	}
 	
 	@Test
 	public void testNone() {
-		assertEquals(Ch1Q3.urlify("supercalifragilisticexpialidocious", 34), "supercalifragilisticexpialidocious");
+		assertEquals("supercalifragilisticexpialidocious", Ch1Q3.urlify("supercalifragilisticexpialidocious", 34));
 	}
 	
 	@Test
 	public void testEmptyString() {
-		assertEquals(Ch1Q3.urlify("", 0), "");
+		assertEquals("", Ch1Q3.urlify("", 0));
 	}
 
 }
