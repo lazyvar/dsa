0f0dc102d4e7fca9d0479813b510b4c5571f268f
Mack Hasz <mack@hasz.email>
2018-03-10 11:29:46 -0500

    Ch2Q2Tests

--- ctci/src/com/hasz/ctci/test/ch2/Ch2Q2Tests.java
+++ ctci/src/com/hasz/ctci/test/ch2/Ch2Q2Tests.java
@@ -4,11 +4,59 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.hasz.ctci.main.ch2.Ch2Q1;
+import com.hasz.ctci.main.ch2.SinglyLinkedList;
+import com.hasz.ctci.main.ch2.SinglyLinkedNode;
+
 public class Ch2Q2Tests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmpty() {
+		int[] list = {};
+		SinglyLinkedList linkedList = new SinglyLinkedList(list);
+		
+		SinglyLinkedNode node = Ch2Q2.kthToLast(3);
+				
+		assertNull(node);
 	}
-
+	
+	@Test
+	public void testOutOfBounds() {
+		int[] list = {1, 2, 3, 4, 5};
+		SinglyLinkedList linkedList = new SinglyLinkedList(list);
+		
+		SinglyLinkedNode node = Ch2Q2.kthToLast(10);
+				
+		assertNull(node);
+	}
+	
+	@Test
+	public void testFirst() {
+		int[] list = {1, 2, 3, 4, 5};
+		SinglyLinkedList linkedList = new SinglyLinkedList(list);
+		
+		SinglyLinkedNode node = Ch2Q2.kthToLast(4);
+				
+		assertNull(node);
+	}
+	
+	@Test
+	public void testMiddle() {
+		int[] list = {1, 2, 3, 4, 5};
+		SinglyLinkedList linkedList = new SinglyLinkedList(list);
+		
+		SinglyLinkedNode node = Ch2Q2.kthToLast(2);
+				
+		assertNull(node);
+	}
+	
+	@Test
+	public void testLast() {
+		int[] list = {1, 2, 3, 4, 5};
+		SinglyLinkedList linkedList = new SinglyLinkedList(list);
+		
+		SinglyLinkedNode node = Ch2Q2.kthToLast(0);
+				
+		assertNull(node);
+	}
 }
